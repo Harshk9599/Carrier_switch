c9e72d60f9e9e2b2ca052061b9d8b4785ffdf1be
Harshk9599 <harshkashyap14321@gmail.com>
2026-02-28 16:56:32 +0530

    new commit

---
+++ backend-week1/project/server.js
@@ -0,0 +1,33 @@
+const express = require("express");
+
+const app = express();
+
+app.use(express.json());
+
+const PORT = 3000;
+
+// app.get("/day2", (req, res) =>{
+//     res.json({ message: "new day2"});
+// });
+
+
+// app.listen(PORT, () =>{
+//     console.log(`Server is running on port ${PORT}`);
+// });
+
+app.post("/user", (req, res)=>{
+    console.log(req.body);
+
+    res.json({
+        message: "user received",
+    });
+});
+
+// app.get("/day2", (req, res) =>{
+//     res.json({ message: "new day2"});
+// });
+
+
+app.listen(PORT, () =>{
+    console.log(`Server is running on port ${PORT}`);
+});
